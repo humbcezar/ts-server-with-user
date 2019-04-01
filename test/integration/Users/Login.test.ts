799b4e3bfb4ad105923e36239e047c989ce1d4b1
Humberto <skateforevis@hotmail.com>
2019-03-31 21:09:06 -0300

    adding login with email

--- test/integration/Users/Login.test.ts
+++ test/integration/Users/Login.test.ts
@@ -8,7 +8,7 @@ import {createHeader} from "../../utils";
 import container from "../../../src/inversify.config";
 
 describe("test/integration/Users/Login.test", () => {
-	it("should login", async () => {
+	it("should login with username", async () => {
 		const password = randomString.generate(12);
 		const user = await createUser({password});
 		const response = await request(app(container))
@@ -19,9 +19,42 @@ describe("test/integration/Users/Login.test", () => {
 		expect(response.status).toBe(200);
 	});
 
+	it("should login with email", async () => {
+		const password = randomString.generate(12);
+		const user = await createUser({password});
+		const response = await request(app(container))
+			.post("/api/users/login")
+			.set(createHeader())
+			.type("form")
+			.send({username: user.email, password: password});
+		expect(response.status).toBe(200);
+	});
+
 	it("should not login", async () => {
 		const response = await request(app(container)).post("/api/users/login");
 		expect(response.status).toBe(400);
 		expect(response.text).toBe("Invalid credentials");
+	});
+
+	it("should not login with wrong username", async () => {
+		const password = randomString.generate(12);
+		const user = await createUser({password});
+		const response = await request(app(container))
+			.post("/api/users/login")
+			.set(createHeader())
+			.type("form")
+			.send({username: user.username + "ast", password: password});
+		expect(response.status).toBe(400);
+	});
+
+	it("should not login with wrong email", async () => {
+		const password = randomString.generate(12);
+		const user = await createUser({password});
+		const response = await request(app(container))
+			.post("/api/users/login")
+			.set(createHeader())
+			.type("form")
+			.send({username: user.email + "ast", password: password});
+		expect(response.status).toBe(400);
 	});
 });
